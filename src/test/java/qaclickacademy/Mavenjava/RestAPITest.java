0845559be8f4a93804d38703ce886e3f3878320c
Sami1401 <parande.sami@yahoo.com>
2022-02-02 14:13:13 +0530

    Commit from Asian Time Zone

--- src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -7,7 +7,11 @@ public class RestAPITest {
 	@Test
 	public void apiAutomation()
 	{
-		System.out.println("apiAutomation");
+		System.out.println("apiAutomation1");
+		System.out.println("apiAutomation2");
+		System.out.println("apiAutomation3");
+		System.out.println("apiAutomation4");
+		
 	}
 
 	@Test
